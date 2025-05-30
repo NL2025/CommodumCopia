43e2d7f77444e698fc35f1730a727022fb777df3
NL2025 <alnouhs@gmail.com>
2025-05-30 14:54:09 +0200

    Update and rename script.js to js/script.js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+fetch('data/products.json')
+    .then(response => response.json())
+    .then(data => {
+        const lijst = document.getElementById('productlijst');
+        data.forEach(product => {
+            const div = document.createElement('div');
+            div.classList.add('product');
+            div.innerHTML = `
+                <a href="product.html?id=${product.product_id}">
+                    <img src="images/${product.afbeelding}" alt="${product.naam}">
+                    <h3>${product.naam}</h3>
+                    <p>${product.beschrijving}</p>
+                    <strong>€ ${product.prijs}</strong>
+                </a>
+            `;
+            lijst.appendChild(div);
+        });
+    })
+    .catch(error => {
+        console.error('Fout bij het laden van producten:', error);
+    });
--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleProductList() {
-    const productList = document.getElementById('product-list');
-    const cart = document.getElementById('cart');
-    productList.style.display = productList.style.display === 'none' ? 'block' : 'none';
-    cart.style.display = 'none'; // Verberg het winkelwagentje bij het tonen van producten
-}
